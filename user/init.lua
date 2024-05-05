3dd81c9a27945093b9ed3092042c25bcf258e809
Amadeo Morán <amadeo.moran@gmail.com>
2024-05-05 19:04:37 +0200

    upd | ni

---
+++ user/init.lua
@@ -0,0 +1,38 @@
+return {
+  lsp = {
+    mappings = function(maps)
+      if maps.n.gd then
+        maps.n.gd[1] = function()
+          vim.lsp.buf.definition()
+        end
+      end
+      if maps.n.gI then
+        maps.n.gI[1] = function()
+          vim.lsp.buf.implementation()
+        end
+      end
+      if maps.n.gr then
+        maps.n.gr[1] = function()
+          vim.lsp.buf.references()
+        end
+      end
+      if maps.n.gy then
+        maps.n.gy[1] = function()
+          vim.lsp.buf.type_definition()
+        end
+      end
+      if maps.n["<leader>lG"] then
+        maps.n["<leader>lG"][1] = function()
+          vim.lsp.buf.workspace_symbol()
+        end
+      end
+      if maps.n["<leader>lR"] then
+        maps.n["<leader>lR"][1] = function()
+          vim.lsp.buf.references()
+        end
+      end
+
+      return maps
+    end,
+  },
+}
